06c0772e7298e9130548939cd21365a522bc4c92
RIVONINGO1 <99362228+RIVONINGO1@users.noreply.github.com>
2025-05-24 21:46:10 +0200

    Added 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,8 @@
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+if (process.argv[2] === undefined || isNaN(process.argv[2]) || parseInt(process.argv[2]) <= 0) {
   console.log("Missing number of occurrences");
 } else {
   let output = "";
-  for (let i = 0; i < Math.abs(parseInt(process.argv[2])); i++) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
     output += "C is fun\n";
   }
   console.log(output.trim());
